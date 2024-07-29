d74901b2a3ec99876bb6c90c466a669c197ccda6
Julian Schmitt <julian.schmitt@ioki.com>
2024-07-29 12:06:16 +0200

    update config

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,19 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
+-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.editing-support.true-zen-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
   -- import/override with your plugins folder
 }
