ed817a9bfe7d50f9e639e6c2d6092bcde703d259
Julian Schmitt <julian.schmitt@ioki.com>
2025-04-15 22:52:46 +0200

    add command to turn selection to pdf

--- lua/polish.lua
+++ lua/polish.lua
@@ -34,8 +34,34 @@ return {
 
       print("PDF saved as: " .. output_file)
     end, { range = true }),
+
+    -- convert visual selection to PDF
+    vim.api.nvim_create_user_command("SelectionToPDFJustified", function(opts)
+      -- Get the file where to save the PDF
+      local output_file = vim.fn.input("Save PDF as: ", "selection.pdf", "file")
+      if output_file == "" then return end
+
+      -- Get the selected lines
+      local start_line = opts.line1
+      local end_line = opts.line2
+
+      -- Create a temporary file with the selection
+      local temp_file = os.tmpname()
+      vim.cmd(start_line .. "," .. end_line .. "write! " .. temp_file)
+
+      -- Convert the temp file to PDF using Weasyprint
+      vim.cmd(
+        "!pandoc " .. temp_file .. " --css='body { text-align: justify; }' --pdf-engine=weasyprint -o " .. output_file
+      )
+
+      -- Remove the temporary file
+      vim.fn.delete(temp_file)
+
+      print("PDF saved as: " .. output_file)
+    end, { range = true }),
+
     -- set keymaps
     vim.keymap.set("n", "<leader>zp", "<cmd>NoteToPDF<CR>", { silent = true }),
-    vim.keymap.set("v", "<leader>zp", "<cmd>VisualToPDF<CR>", { silent = true }),
+    vim.keymap.set("v", "<leader>zp", "<cmd>SelectionToPDF<CR>", { silent = true }),
   },
 }
